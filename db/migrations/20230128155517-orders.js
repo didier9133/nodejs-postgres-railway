d6eac7f7559d6df81a85e5bac0a8dc4f37be1830
Didier Contreras <didier9133@gmail.com>
2023-01-30 15:01:37 -0400

    fix datatype.Virtual on order migrations

--- db/migrations/20230128155517-orders.js
+++ db/migrations/20230128155517-orders.js
@@ -1,13 +1,35 @@
 'use strict';
-const {
-  TABLE_NAME: ordersTableName,
-  OrderSchema,
-} = require('../models/order.models');
+const { DataTypes, Sequelize } = require('sequelize');
+const { TABLE_NAME: ordersTableName } = require('../models/order.models');
+const { TABLE_NAME: customerTableName } = require('../models/customer.models');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ordersTableName, OrderSchema);
+    await queryInterface.createTable(ordersTableName, {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW(),
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: customerTableName,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+    });
   },
 
   async down(queryInterface) {
